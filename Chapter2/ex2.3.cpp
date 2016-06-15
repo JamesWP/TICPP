988d1189b3db6dfb5fccb80f455b8c749a346860
james_peach01 <james_peach01@hotmail.co.uk>
2016-06-15 06:26:15 -0400

    finished ex2.3

---
+++ Chapter2/ex2.3.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if(argc<2)
+  {
+    cout << "Usage: ex2.3 [file-name ...]" << endl;
+    return 1;
+  }
+  
+  ifstream file;
+  
+  file.open(argv[1]);
+  
+  string word;
+  int numWords = 0;
+  while(file>>word) numWords++;
+  
+  cout << numWords << endl;
+  
+  return 0;
+}
