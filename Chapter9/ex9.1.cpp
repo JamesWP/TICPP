ca60b14ffdc9a9da76cb25ff4cbddd86d036b520
JamesWP <james_peach01@hotmail.co.uk>
2016-06-15 18:13:23 +0100

    added chapter 9

---
+++ Chapter9/ex9.1.cpp
@@ -0,0 +1,25 @@
+#define F (x) (x + 1)
+#define V(x) (x + 1)
+
+
+
+//F(1)
+//expands to
+//(x) (x + 1)(1)
+
+//F (1)
+//expands to
+//(x) (x + 1) (1)
+
+//V(1)
+//expands to
+//(1 + 1)
+
+//V (1)
+//expands to
+//(1 + 1)
+
+int main(){}
+
+
+
